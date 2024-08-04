b3ea661508a57d6fa99eafc14849eff263041aaf
Panthomema <71283471+Panthomema@users.noreply.github.com>
2024-08-04 21:14:45 +0200

    Refactor Library

--- scripts/controllers/Library.js
+++ scripts/controllers/Library.js
@@ -1,38 +1,46 @@
 import Book from "./Book.js";
 
-function Library() {
-  // Get the data on construction
-  const storedData = JSON.parse(localStorage.getItem('books')) ?? [];
-  this.books = storedData.map(data => new Book(data));
+class Library {
+  constructor() {
+    // Get the data on construction
+    const storedData = JSON.parse(localStorage.getItem('books')) ?? [];
+    this.books = storedData.map(data => new Book(data));
+  }
+
+  get(index) {
+    return this.books[index];
+  }
+
+  has(book) {
+    return this.books
+      .find(own => JSON.stringify(own) === JSON.stringify(book)) !== undefined;
+  }
+
+  add(book) {
+    this.books.push(book);
+  }
+
+  remove(index) {
+    this.books.splice(index, 1);
+  }
+
+  sort() {
+    return this.books.sort((a, b) => {
+      if (a.isRead === b.isRead) return 0;
+      return (a.isRead) ? 1 : -1;
+    });
+  }
+  
+  save() {
+    localStorage.setItem('books', JSON.stringify(this.books));
+  }
 }
 
-Library.prototype.get = function (index) {
-  return this.books[index];
-}
 
-Library.prototype.has = function (book) {
-  return this.books
-    .find(own => JSON.stringify(own) === JSON.stringify(book)) !== undefined;
-}
 
-Library.prototype.add = function (book) {
-  this.books.push(book);
-}
 
-Library.prototype.remove = function (index) {
-  this.books.splice(index, 1);
-}
 
-Library.prototype.sort = function () {
-  return this.books.sort((a, b) => {
-    if (a.isRead === b.isRead) return 0;
-    return (a.isRead) ? 1 : -1;
-  });
-}
 
-Library.prototype.save = function () {
-  localStorage.setItem('books', JSON.stringify(this.books));
-}
 
 export default Library;
  
