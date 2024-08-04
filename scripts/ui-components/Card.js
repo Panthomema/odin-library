2d577e2f129ea3ea821962b1e13ab3c840f56223
Panthomema <71283471+Panthomema@users.noreply.github.com>
2024-08-04 21:20:30 +0200

    Refactor Card

--- scripts/ui-components/Card.js
+++ scripts/ui-components/Card.js
@@ -1,51 +1,68 @@
 import Button from "./Button.js";
-import { 
-  SVG_COMPLETED_PATH_D, SVG_DELETE_PATH_D, SVG_PENDING_PATH_D 
+import {
+  SVG_COMPLETED_PATH_D,
+  SVG_DELETE_PATH_D,
+  SVG_PENDING_PATH_D,
 } from "../svgData.js";
 
+class Card {
+  constructor(book) {
+    this.htmlElement = document.createElement("div");
+    this.htmlElement.classList.add("book-card");
 
-function Card(book) {
-  this.htmlElement = document.createElement('div');
-  this.htmlElement.classList.add('book-card');
+    // Render content
+    const content = Array.from({ length: 4 }, () =>
+      document.createElement("p")
+    );
+    content[0].textContent = book.title;
+    content[0].classList.add("book-title");
+    content[1].textContent =
+      `${book.author || "Unknown author"}` +
+      `${book.publishYear ? ", " + book.publishYear : ""}`;
+    content[2].textContent = book.category || "Uncathegorized";
+    content[3].textContent = `${book.numPages || "N/D"} pages`;
 
-  // Render content
-  const content = Array.from({ length: 4 }, () => document.createElement('p'));
-  content[0].textContent = book.title;
-  content[0].classList.add('book-title');
-  content[1].textContent = `${book.author || 'Unknown author'}` + 
-    `${(book.publishYear) ? ', ' + book.publishYear : ''}`;
-  content[2].textContent = book.category || 'Uncathegorized';
-  content[3].textContent = `${book.numPages || 'N/D'} pages`;
+    content.forEach((line) => {
+      this.htmlElement.appendChild(line);
+    });
 
-  content.forEach(line => {
-    this.htmlElement.appendChild(line);
-  });
+    // Create buttns
+    this.deleteButton = new Button(
+      "library-danger",
+      SVG_DELETE_PATH_D,
+      "delete."
+    );
+    this.pendingButton = new Button(
+      "book-pending",
+      SVG_PENDING_PATH_D,
+      "read."
+    );
+    this.completedButton = new Button(
+      "book-completed",
+      SVG_COMPLETED_PATH_D,
+      "notRead."
+    );
 
-  // Create buttns
-  this.deleteButton = new Button('library-danger', SVG_DELETE_PATH_D, 'delete.');
-  this.pendingButton = new Button('book-pending', SVG_PENDING_PATH_D, 'read.');
-  this.completedButton = new Button(
-    'book-completed', SVG_COMPLETED_PATH_D, 'notRead.'
-  );
+    const controlArea = document.createElement("div");
+    controlArea.classList.add("card-control");
 
-  const controlArea = document.createElement('div');
-  controlArea.classList.add('card-control');
+    controlArea.appendChild(
+      book.isRead
+        ? this.completedButton.htmlElement
+        : this.pendingButton.htmlElement
+    );
+    controlArea.appendChild(this.deleteButton.htmlElement);
 
-  controlArea.appendChild((book.isRead) 
-    ? this.completedButton.htmlElement 
-    : this.pendingButton.htmlElement
-  );
-  controlArea.appendChild(this.deleteButton.htmlElement);
+    // Add listeners on creation (method defined on main)
+    this.addButtonListeners();
 
-  // Add listeners on creation (method defined on main)
-  this.addButtonListeners();
-
-  this.htmlElement.appendChild(controlArea);
-}
-
-Card.prototype.setDataIndex = function (index) {
-  this.htmlElement.dataset.index = index;
-  return this;
+    this.htmlElement.appendChild(controlArea);
+  }
+  
+  setDataIndex(index) {
+    this.htmlElement.dataset.index = index;
+    return this;
+  }
 }
 
 export default Card;
