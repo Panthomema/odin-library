b335544408b6b4d50ed39a4482c8a6f6e9ae3186
Panthomema <71283471+Panthomema@users.noreply.github.com>
2024-08-04 21:07:21 +0200

    Refactor book class

--- scripts/controllers/Book.js
+++ scripts/controllers/Book.js
@@ -1,14 +1,15 @@
-function Book({ title, author, publishYear, category, numPages, isRead }) {
-  this.title = title;
-  this.author = author;
-  this.publishYear = publishYear;
-  this.category = category;
-  this.numPages = numPages;
-  this.isRead = isRead ?? false;
-}
-
-Book.prototype.toggleReadState = function () {
-  this.isRead = ! this.isRead;
+class Book {
+  constructor({ title, author, publishYear, category, numPages, isRead }) {
+    this.title = title;
+    this.author = author;
+    this.publishYear = publishYear;
+    this.category = category;
+    this.numPages = numPages;
+    this.isRead = isRead ?? false;
+  }
+  toggleReadState() {
+    this.isRead = !this.isRead;
+  }
 }
 
 export default Book;
