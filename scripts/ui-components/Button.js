dde2f2c756e7d266ca087335f8ccf42fea1f58ff
Panthomema <71283471+Panthomema@users.noreply.github.com>
2024-08-04 21:17:59 +0200

    Refactor Button

--- scripts/ui-components/Button.js
+++ scripts/ui-components/Button.js
@@ -1,18 +1,20 @@
 import { addTooltip } from "../functions.js";
 import SVG from "./SVG.js";
 
-function Button(className, pathData, tooltipText) {
-  const svg = new SVG(pathData).node;
+class Button {
+  constructor(className, pathData, tooltipText) {
+    const svg = new SVG(pathData).node;
 
-  this.htmlElement = document.createElement('button');
-  this.htmlElement.classList.add(className);
-  this.htmlElement.appendChild(svg);
+    this.htmlElement = document.createElement('button');
+    this.htmlElement.classList.add(className);
+    this.htmlElement.appendChild(svg);
 
-  addTooltip(this.htmlElement, tooltipText);
-}
-
-Button.prototype.addListener = function (event, callback) {
-  this.htmlElement.addEventListener(event, callback);
+    addTooltip(this.htmlElement, tooltipText);
+  }
+  
+  addListener(event, callback) {
+    this.htmlElement.addEventListener(event, callback);
+  }
 }
 
 export default Button;
