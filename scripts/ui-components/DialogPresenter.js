4078a4b301ab837743ffa9efee2cf14460978810
Panthomema <71283471+Panthomema@users.noreply.github.com>
2024-08-04 21:23:09 +0200

    Refactor DialogPresenter

--- scripts/ui-components/DialogPresenter.js
+++ scripts/ui-components/DialogPresenter.js
@@ -1,50 +1,51 @@
-function DialogPresenter (selector) {
-  this.htmlElement = document.querySelector(selector);
+class DialogPresenter {
+  constructor(selector) {
+    this.htmlElement = document.querySelector(selector);
 
-  // Detects if the event is outside/inside of the dialog, in order to close it
-  this.clickOutsideHandler = event => {
-    event.stopPropagation();
+    // Detects if the event is outside/inside of the dialog, in order to close it
+    this.clickOutsideHandler = event => {
+      event.stopPropagation();
 
-    const rect = this.htmlElement.getBoundingClientRect();
-    const isInDialog = 
-      rect.left <= event.clientX && 
-      event.clientX <= rect.right && 
-      rect.top <= event.clientY && 
-      event.clientY <= rect.bottom;
+      const rect = this.htmlElement.getBoundingClientRect();
+      const isInDialog = rect.left <= event.clientX &&
+        event.clientX <= rect.right &&
+        rect.top <= event.clientY &&
+        event.clientY <= rect.bottom;
 
-    if (! isInDialog) {
-      this.hide();
-    }
-  }
-}
-
-// Shows the modal with transition (prevents getting fired by bubbling events)
-DialogPresenter.prototype.show = function() {
-  const handler = event => {
-    if (event.target === this.htmlElement) {
-      document.addEventListener('click', this.clickOutsideHandler);
-      this.htmlElement.removeEventListener('transitionend', handler);
-    }
-  };
-  this.htmlElement.addEventListener('transitionend', handler);
-
-  this.htmlElement.showModal();
-  this.htmlElement.classList.remove('invisible');   
-}
-
-// Hides the modal with transition (prevents getting fired by bubbling events)
-DialogPresenter.prototype.hide = function () {
-  const handler = event => {
-    if (event.target === this.htmlElement) {
-      event.currentTarget.close();
-      this.htmlElement.removeEventListener('transitionend', handler);
-    }
+      if (!isInDialog) {
+        this.hide();
+      }
+    };
   }
 
-  this.htmlElement.addEventListener('transitionend', handler);
+  // Shows the modal with transition (prevents getting fired by bubbling events)
+  show() {
+    const handler = event => {
+      if (event.target === this.htmlElement) {
+        document.addEventListener('click', this.clickOutsideHandler);
+        this.htmlElement.removeEventListener('transitionend', handler);
+      }
+    };
+    this.htmlElement.addEventListener('transitionend', handler);
 
-  document.removeEventListener('click', this.clickOutsideHandler);
-  this.htmlElement.classList.add('invisible');
+    this.htmlElement.showModal();
+    this.htmlElement.classList.remove('invisible');
+  }
+
+  // Hides the modal with transition (prevents getting fired by bubbling events)
+  hide() {
+    const handler = event => {
+      if (event.target === this.htmlElement) {
+        event.currentTarget.close();
+        this.htmlElement.removeEventListener('transitionend', handler);
+      }
+    };
+
+    this.htmlElement.addEventListener('transitionend', handler);
+
+    document.removeEventListener('click', this.clickOutsideHandler);
+    this.htmlElement.classList.add('invisible');
+  }
 }
 
 export default DialogPresenter;
