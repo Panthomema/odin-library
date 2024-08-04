202888352a8c46012fe49f5c4f1cb87b1ae5359a
Panthomema <71283471+Panthomema@users.noreply.github.com>
2024-08-04 21:26:45 +0200

    Refactor SVG class

--- scripts/ui-components/SVG.js
+++ scripts/ui-components/SVG.js
@@ -1,11 +1,13 @@
-function SVG(pathData) {
-  const path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
-  path.setAttribute('d', pathData);
+class SVG {
+  constructor(pathData) {
+    const path = document.createElementNS("http://www.w3.org/2000/svg", "path");
+    path.setAttribute("d", pathData);
 
-  this.node = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
-  this.node.setAttribute('xmlns', 'http://www.w3.org/2000/svg'); // Optional
-  this.node.setAttribute('viewBox', '0 -960 960 960');
-  this.node.appendChild(path);
+    this.node = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+    this.node.setAttribute("xmlns", "http://www.w3.org/2000/svg"); // Optional
+    this.node.setAttribute("viewBox", "0 -960 960 960");
+    this.node.appendChild(path);
+  }
 }
 
 export default SVG;
